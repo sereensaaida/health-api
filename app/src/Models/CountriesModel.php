6216fea12428a141fc7736c6c640a3ac8790c40b
%sereensaaida% <%sda.sereen@gmail.com%>
2024-10-13 14:38:35 -0400

    completed sorting for countries and guidelines

--- app/src/Models/CountriesModel.php
+++ app/src/Models/CountriesModel.php
@@ -10,48 +10,69 @@ class CountriesModel extends BaseModel
     {
         parent::__construct($pdo);
     }
+
+    //* Get /countries
     public function getCountries(array $filter_params = []): mixed
     {
+        //*Declaring named parameters and initiating query
         $named_params = [];
         $sql = "SELECT * FROM countries WHERE 1";
 
+        //*FILTERING:
+        //*Filter implementation for each field, check if a value is present and concats it to SQL
+        //name
         if (isset($filter_params['name'])) {
             $sql .= " AND name LIKE CONCAT(:name,'%')";
             $named_params['name'] = $filter_params['name'];
         }
-
+        //population
         if (isset($filter_params['population'])) {
             $sql .= " AND population LIKE (:population,'%')";
             $named_params['population'] = $filter_params['population'];
         }
-
+        //vegetarian
         if (isset($filter_params['vegetarian_percentage'])) {
             $sql .= " AND vegetarian_percentage LIKE CONCAT(:vegetarian_percentage,'%')";
             $named_params['vegetarian_percentage'] = $filter_params['vegetarian_percentage'];
         }
-
+        //daily calorie intake
         if (isset($filter_params['daily_calorie_intake'])) {
             $sql .= " AND daily_calorie_intake LIKE CONCAT(:daily_calorie_intake,'%')";
             $named_params['daily_calorie_intake'] = $filter_params['daily_calorie_intake'];
         }
-
+        //consumed dishes
         if (isset($filter_params['consumed_dishes'])) {
             $sql .= " AND consumed_dishes LIKE CONCAT(:consumed_dishes,'%')";
             $named_params['consumed_dishes'] = $filter_params['consumed_dishes'];
         }
-
+        //food culture
         if (isset($filter_params['food_culture'])) {
             $sql .= " AND food_culture LIKE CONCAT(:food_culture,'%')";
             $named_params['food_culture'] = $filter_params['food_culture'];
         }
-
+        //nutritional deficiency
         if (isset($filter_params['nutritional_deficiency'])) {
             $sql .= " AND nutritional_deficiency LIKE CONCAT(:nutritional_deficiency,'%')";
             $named_params['nutritional_deficiency'] = $filter_params['nutritional_deficiency'];
         }
+
+        //*SORTING:
+        //*Retrieving all sorting/order params
+        $sortBy = isset($filter_params['sort_by']) ? $filter_params['sort_by'] : 'country_id';
+        $order = isset($filter_params['order']) ? $filter_params['order'] : 'asc';
+
+        //*Sorting validation
+        $validSortingParameters = ['population', 'name', 'vegetarian_percentage', 'daily_calorie_intake', 'consumed_dishes', 'food_culture', 'nutritional_deficiency'];
+        $sortBy = in_array($sortBy, $validSortingParameters) ? $sortBy : 'name';
+        $order = ($order === 'desc') ? 'desc' : 'asc';
+
+        //*Adding it so SQL
+        $sql .= " ORDER BY $sortBy $order";
         return   (array)$this->fetchAll($sql, $named_params);
     }
 
+
+    //* Get /country/{country_id}
     public function getCountryId(string $country_id): mixed
     {
         $query = "SELECT * FROM countries WHERE country_id = :country_id";
@@ -62,6 +83,7 @@ class CountriesModel extends BaseModel
         return $country_info;
     }
 
+    //*Subcollection implementation /country/guidelines/{country_id}
     public function getCountryGuidelines($country_id): mixed
     {
         $country = $this->getCountryId($country_id);
@@ -79,24 +101,14 @@ class CountriesModel extends BaseModel
         return $result;
     }
 
+    //*(Build 2): Create Country implementation
     public function insertCountry(array $country): mixed
     {
-        $this->insert(
+        $last_id = $this->insert(
             "Countries",
             $country
         );
-        
-        return "blah";
-    }
 
-    public function updateCountry(array $country): mixed
-    {
-        $this->update(
-            "Countries",
-            $country,
-            ["country_id => $country[country_id]"]
-        );
-
-        return "blah";
+        return $last_id;
     }
 }
--- app/src/Models/GuidelinesModel.php
+++ app/src/Models/GuidelinesModel.php
@@ -10,52 +10,70 @@ class GuidelinesModel extends BaseModel
     {
         parent::__construct($pdo);
     }
+
+    //*Get /countries
     public function getGuidelines(array $filter_params = []): mixed
     {
+
+        //*Declaring named parameters and initiating query
         $named_params = [];
         $sql = "SELECT * FROM guidelines WHERE 1";
 
+        //*FILTERING:
+        //*Filter implementation for each field, check if a value is present and concats it to SQL
+        //country
         if (isset($filter_params['country_id'])) {
             $sql .= " AND country_id LIKE  CONCAT(:country_id,'%')";
             $named_params['country_id'] = $filter_params['country_id'];
         }
-
+        //calorie intake
         if (isset($filter_params['calorie_intake'])) {
             $sql .= " AND calorie_intake LIKE  CONCAT(:calorie_intake,'%')";
             $named_params['calorie_intake'] = $filter_params['calorie_intake'];
         }
-
+        //protein intake
         if (isset($filter_params['protein_intake'])) {
             $sql .= " AND protein_intake LIKE  CONCAT(:protein_intake,'%')";
             $named_params['protein_intake'] = $filter_params['protein_intake'];
         }
-
+        //fats
         if (isset($filter_params['fats'])) {
             $sql .= " AND fats LIKE  CONCAT(:fats,'%')";
             $named_params['fats'] = $filter_params['fats'];
         }
-
+        //carbohydrates
         if (isset($filter_params['carbohydrates'])) {
             $sql .= " AND carbohydrates LIKE  CONCAT(:carbohydrates,'%')";
             $named_params['carbohydrates'] = $filter_params['carbohydrates'];
         }
-
+        //servings per day
         if (isset($filter_params['servings_per_day'])) {
             $sql .= " AND servings_per_day LIKE  CONCAT(:servings_per_day,'%')";
             $named_params['servings_per_day'] = $filter_params['servings_per_day'];
         }
-
+        //guideline notes
         if (isset($filter_params['guideline_notes'])) {
             $sql .= " AND guideline_notes LIKE  CONCAT(:guideline_notes,'%')";
             $named_params['guideline_notes'] = $filter_params['guideline_notes'];
         }
 
+        //*SORTING:
+        //*Retrieving all sorting/order params
+        $sortBy = isset($filter_params['sort_by']) ? $filter_params['sort_by'] : 'guidelines_id';
+        $order = isset($filter_params['order']) ? $filter_params['order'] : 'asc';
 
+        //*Sorting validation
+        $validSortingParameters = ['country_id', 'calorie_intake', 'protein_intake', 'fats', 'carbohydrates', 'servings_per_day', 'guideline_notes'];
+        $sortBy = in_array($sortBy, $validSortingParameters) ? $sortBy : 'country_id';
+        $order = ($order === 'desc') ? 'desc' : 'asc';
+
+        //*Adding it to SQL
+        $sql .= " ORDER BY $sortBy $order";
         $guidelines = (array) $this->fetchAll($sql, $named_params);
-
         return $guidelines;
     }
 
+    //* Get /guideline/{guideline_id}
     public function getGuidelinesId(string $guideline_id): mixed
     {
 
@@ -65,6 +83,5 @@ class GuidelinesModel extends BaseModel
             ["guideline_id" => $guideline_id]
         );
         return $guidelines_info;
-        
     }
 }
