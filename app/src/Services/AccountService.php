b3c7f7489b22989fcefb84789bfc61a6b9fda042
Damiano Miloncini <miloncinid@gmail.com>
2024-11-22 13:07:58 -0500

    User registration implementation
    Began the implementation of user registration: service, mode, and controller.

---
+++ app/src/Services/AccountService.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Services;
+
+use App\Core\PasswordTrait;
+use App\Core\Result;
+use App\Validation\Validator;
+use App\Models\AccountModel;
+
+class AccountService
+{
+    use PasswordTrait;
+
+    public function __construct(private AccountModel $account_model)
+    {
+        $this->account_model = $account_model;
+    }
+
+    /**
+     * Service for creating a new food
+     *
+     * @param array $new_food The array of food information from the request body
+     * @return Result Returning the result in JSON format and calling the appropriate model method
+     */
+    public function createUser(array $new_user): Result
+    {
+        //* Step 1) Using Valitron, validate the data
+        $data = array(
+            "first_name" => $new_user['first_name'],
+            "last_name" => $new_user['last_name'],
+            "email" => $new_user['email'],
+            "password" => $this->cryptPassword($new_user['password']),
+            "role" => $new_user['role'],
+        );
+
+        $rules = array(
+            'user_id' => [
+                'integer',
+                ['min', 1]
+            ],
+            'first_name' => array(
+                'required',
+                array('lengthMin', 4)
+            ),
+            'last_name' => array(
+                'required',
+                array('lengthMin', 4)
+            ),
+            'email' => array(
+                'required',
+                array('lengthMin', 4)
+            ),
+            'password' => array(
+                'required',
+                array('lengthMin', 4)
+            ),
+            'created_at' => array(
+                'required',
+                array('lengthMin', 4)
+            )
+        );
+
+        //* Step 2) Insert into the database
+        $validator = new Validator($data, [], 'en');
+
+        // Fix this:
+        $validator->mapFieldsRules($rules);
+
+        if ($validator->validate()) {
+            $this->account_model->insertUser($new_user);
+            return Result::success("User has been inserted!");
+        } else {
+            return Result::fail("Data not valid.");
+        }
+    }
+}
